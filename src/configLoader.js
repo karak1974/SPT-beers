a8bbfd5cdc9596adc12d60ade49e4a9defebf559
Victor <hoodrich10135@gmail.com>
2024-08-16 19:29:34 -0700

    removed bad files

--- src/configLoader.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ConfigLoader = void 0;
-class ConfigLoader {
-    config = require("../config/config.jsonc");
-}
-exports.ConfigLoader = ConfigLoader;
-//# sourceMappingURL=configLoader.js.map
